47eb7f9cbd71ea50fa50b45b382801cdd0ce7d1d
ambar <ambargupta11@gmail.com>
2022-01-20 19:28:28 +0530

    Login with username and password saved in mysql

---
+++ java/com/springboot/springsecurity/User.java
@@ -0,0 +1,43 @@
+package com.springboot.springsecurity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="security_user")
+public class User {
+	@Id
+	private int id;
+	private String username;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return username;
+	}
+	public void setUserName(String userName) {
+		this.username = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public User(int id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+	public User() {
+		super();
+	}
+	
+	
+}
